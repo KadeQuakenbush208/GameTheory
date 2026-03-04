7cd5ef71ba8172349bc6b38e44da6a836cc95bc1
KadeQuakenbush <jedimasterkade@gmail.com>
2026-03-04 12:50:17 +0000

    Changed setup to use subplots instead of separate plots feat: Program now outputs two .jpg files, time_series.jpg and phase_plane.jpg, which are plots consisting of a grid of subplots with shared axes. feat: Added markers to the first and last points of the phase plane plot fix: The correct strategy is now switched when a switch occurs feat: Changed the values contained by `ws` and `ss` to provide better insights docs: Added some more comments for clarity feat: Removed some print statements used for debugging chore: Deleted phase_plane and time_series folders since they are no longer necessary; added .gitkeep to /results/birth-death

--- src/main.py
+++ src/main.py
@@ -8,18 +8,6 @@ def debug(message, debugging=DEBUG):
     if debugging:
         print(message)
 
-def p_switch(A, B, w, s):
-    """Get the probability that agent A switches to agent B's strategy given their payoffs and other parameters.
-    Args:
-        A: Agent A's strategy.
-        B: Agent B's strategy.
-        w: Parameter changes how influential the expected payoff difference is.
-        s: Used to calculate the maximum possible payoff difference of 1 + s.
-    Returns:
-        float: Probability that agent A switches to agent B's strategy
-    """
-    return 1/2 + w * (get_expected_payoff(A, agents, s) - get_expected_payoff(B, agents, s)) / (2 * (1 + s))
-
 def get_payoff(A, B, s: float):
     """Get the payoff for an game of non-zero-sum rock-paper-scissors.
     Args:
@@ -54,9 +42,9 @@ def get_expected_payoff(focal_strat, strats, s: float):
     Args:
         focal_strat: The focal player's strategy.
         strats ([]): All players' strategies, including the focal player's.
-        s (float): The payoff for losing
+        s (float): The payoff for losing.`
     Returns:
-        float: Expected payoff of the focal player's strategy
+        float: Expected payoff of the focal player's strategy.
     """
     opp_strats = strats[:]
     opp_strats.remove(focal_strat)
@@ -64,62 +52,96 @@ def get_expected_payoff(focal_strat, strats, s: float):
 
     return sum(payoffs)/len(payoffs)
 
+def p_switch(A, B, w, s):
+    """Get the probability that agent A switches to agent B's strategy given their payoffs and other parameters.
+    Args:
+        A: The focal agent's strategy.
+        B: An opposing agent's strategy.
+        w: Parameter changes how influential the expected payoff difference is.
+        s: Used to calculate the maximum possible payoff difference of 1 + s.
+    Returns:
+        float: Probability that the agent playing strategy B switches to strategy A.
+    """
+    return 1/2 + w * (get_expected_payoff(A, agents, s) - get_expected_payoff(B, agents, s)) / (2 * (1 + s))
+
+# Parameters
+
 strats = ["R", "P", "S"]
 N = 1500 # Population size
-runtime = 100000 # Number of epochs
-ws = [0.25, 0.5, 0.75]
-ss = [0.75, 1.5, 3]
+init_agents = [strats[i % 3] for i in range(N)] # Initial conditions - even split of all strategies
+runtime = 1000 # Number of epochs
+ws = [0.0, 0.5, 1.0] # 0 <= w <= 1
+ss = [0.25, 1.0, 4.0]
 
-for permut in [(w, s) for w in ws for s in ss]:
-    print(f"Running setup w = {permut[0]}, s = {permut[1]}...")
-    agents = [strats[i % 3] for i in range(N)]
-    w = permut[0]
-    s = permut[1]
+# Evolution
 
-    results = { # Index is time
-        "R": [], # Number of agents playing R
-        "P": [], # Number of agents playing P
-        "S": [] # Number of agents playing S
-    }
+fig, axes = [None, None], [None, None]
+for i in range(2):
+    fig[i], axes[i] = plt.subplots(
+        nrows=len(ws),
+        ncols=len(ss),
+        figsize=(8.5, 8.5),
+        sharex=True,
+        sharey=True,
+        gridspec_kw={"wspace": 0.1, "hspace": 0.1},
+    )
 
-    for i in range(runtime):
-        A = random.choice(agents)
-        B = random.choice(agents)
+for w_i in range(len(ws)):
+    for s_i in range(len(ss)):
+        w, s = ws[w_i], ss[s_i]
+        print(f"Running setup {w_i*len(ws) + s_i + 1}/{len(ws)*len(ss)} (w = {w}, s = {s}) for {runtime} epochs...")
+        agents = init_agents[:]
 
-        if A != B:
-            p = p_switch(A, B, w, s)
-            debug(f"Probability that agent A switches their strategy ({A}) to B's ({B}): {p}")
+        results = { # Index is time
+            "R": [], # Number of agents playing R
+            "P": [], # Number of agents playing P
+            "S": [] # Number of agents playing S
+        }
 
-            if p > random.random():
-                agents.remove(A) # Death of agent A's strategy
-                agents.append(B) # Replication of agent B's strategy
-                debug("Agent A has switched strategies")
-            else:
-                pass
-                debug("Agent A has not switched strategies")
-        else:
-            # print(f"No switch: both agents are using the same strategy ({A})")
-            pass
-            
-        results["R"].append(agents.count("R"))
-        results["P"].append(agents.count("P"))
-        results["S"].append(agents.count("S"))
+        for i in range(runtime):
+            A = agents.pop(random.randint(0, len(agents)) - 1) # To prevent selection of the same agent for B
+            B = agents[random.randint(0, len(agents)) - 1]
+            agents.append(A)
 
-    plt.plot(results["R"], label="R")
-    plt.plot(results["P"], label="P")
-    plt.xlabel("Time (epochs)")
-    plt.ylabel("Number of Agents")
-    plt.title(f"Evolution of Rock-Paper-Scissors Strategies Over Time\n(w = {w}, s = {s})")
-    plt.legend()
-    # plt.ylim(0, N)
-    plt.savefig(f"results\\birth-death\\time_series\\w-{w},s-{s}.jpg")
-    plt.clf()
+            if A != B:
+                p = p_switch(A, B, w, s)
 
-    plt.plot(results["R"], results["P"])
-    plt.xlabel("Number of Agents Playing R")
-    plt.ylabel("Number of Agents Playing P")
-    plt.title(f"Phase Plane of Evolution of Rock-Paper-Scissors Strategies\n(w = {w}, s = {s})")
-    # plt.xlim(0, N)
-    # plt.ylim(0, N)
-    plt.savefig(f"results\\birth-death\\phase_plane\\w-{w},s-{s}.jpg")
-    plt.clf()
+                if p > random.random():
+                    agents.remove(B) # Death of a strategy B
+                    agents.append(A) # Replication of strategy A
+                
+            results["R"].append(agents.count("R"))
+            results["P"].append(agents.count("P"))
+            results["S"].append(agents.count("S"))
+
+        # Time series
+
+        axes[0][w_i, s_i].plot(results["R"], label="R")
+        axes[0][w_i, s_i].plot(results["P"], label="P")
+        if w_i == len(ws) - 1: axes[0][w_i, s_i].set_xlabel(f"s = {s:.2f}") # Only label if it is in the bottom row
+        if s_i == 0: axes[0][w_i, s_i].set_ylabel(f"w = {w:.2f}") # Only label if it is in the leftmost column
+        fig[0].supxlabel("Time (epochs)")
+        fig[0].supylabel("Number of Agents")
+        fig[0].suptitle("Evolution of Rock-Paper-Scissors Strategies Over Time", y=0.95, fontsize=20)
+
+        # Phase plane
+
+        axes[1][w_i, s_i].plot(
+            results["R"],
+            results["P"],
+            markevery=[0, -1],
+            marker='.',
+            markerfacecolor='k',
+            markeredgecolor='k'
+        )
+        if w_i == len(ws) - 1: # Only label if it is the bottom row
+            axes[1][w_i, s_i].set_xlabel(f"s = {s:.2f}")
+        if s_i == 0: # Only label if it is the leftmost column
+            axes[1][w_i, s_i].set_ylabel(f"w = {w:.2f}")
+        fig[1].supxlabel("Number of Agents Playing R")
+        fig[1].supylabel("Number of Agents Playing P")
+        fig[1].suptitle(f"Phase Plane of Evolution of Rock-Paper-Scissors Strategies", y=0.95, fontsize=20)
+
+fig[0].savefig(f"results\\birth-death\\time_series.jpg")
+
+fig[1].savefig(f"results\\birth-death\\phase_plane.jpg")
